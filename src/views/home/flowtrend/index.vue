d6e07559a558235964edac076df964bf466e74ad
xyc <1365884247@qq.com>
2021-03-04 17:45:39 +0800

    添加lenged

--- src/views/home/flowtrend/index.vue
+++ src/views/home/flowtrend/index.vue
@@ -1,13 +1,38 @@
 <!--客流走势-->
 <template>
   <div class="flowtrend">
-客流走势
+    客流走势
+    <Lenged v-for="item in lengedData" :key="item.id" :data="item"/>
   </div>
 </template>
 
 <script>
+import Lenged from '@/components/lenged'
 export default {
-
+  data () {
+    return {
+      lengedData: [
+        {
+          id: 1,
+          color: '#FA010A',
+          title: '今日 106人'
+        },
+        {
+          id: 2,
+          color: '#FF7B40',
+          title: '昨日 32人/261人'
+        },
+        {
+          id: 3,
+          color: '#E5006B',
+          title: '前日 87人/258人'
+        }
+      ]
+    }
+  },
+  components: {
+    Lenged
+  }
 }
 </script>
 
---
+++ src/views/user/components/item.vue
@@ -0,0 +1,16 @@
+<!--每一个item项-->
+<template>
+  <div>
+每一个item项
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
