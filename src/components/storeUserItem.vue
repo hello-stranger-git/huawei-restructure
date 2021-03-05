015cf52c90ca91dea1b6eec7b0caf21553640156
LZ <2706253331@qq.com>
2021-03-05 17:56:36 +0800

    完成storeUserItem组件

--- src/components/storeUserItem.vue
+++ src/components/storeUserItem.vue
@@ -1,7 +1,26 @@
 <!--当日进店每一个用户-->
 <template>
-  <div>
-当日进店每一个用户
+  <div class="storeUserItem">
+    <div class="storeUserItem_img">
+      <img class="userIcon" :src="data.icon ? data.icon : defaultTouXaing"/>
+      <img v-if="data.vip" class="vipIcon" :src="vipIcon"/>
+    </div>
+    <!-- 时间 -->
+    <p style="color:#787878">
+      <van-icon :name="timeIcon" />
+      <span>{{data.date}}</span>
+    </p>
+    <!-- 年龄 -->
+    <p style="color:#787878">
+      <van-icon :name="data.sex === 0 ? womanIcon: manIcon" />
+      <span>{{data.age}}</span>
+    </p>
+    <!-- 到店次数及是否跨店 -->
+    <p style="color:#343434">
+      <van-icon :name="data.crossStore ?  storeIcon : ''" />
+      <span v-if="data.count>1" :class="data.crossStore ? '': 'noIcon'">{{data.count}}次到店</span>
+      <span v-else :class="data.crossStore ? '': 'noIcon'" style="color:#4A92FF">新客</span>
+    </p>
   </div>
 </template>
 
@@ -11,10 +30,62 @@ export default {
     data: {
       type: Object
     }
+  },
+  data () {
+    return {
+      vipIcon: require('@/assets/image/storeUserItem/vipIcon.png'), // vip图标
+      defaultTouXaing: require('@/assets/image/storeUserItem/defaultTouXaing.png'), // 用户图片不显示时默认图标
+      manIcon: require('@/assets/image/storeUserItem/manIcon.png'), // 男性图标
+      womanIcon: require('@/assets/image/storeUserItem/womanIcon.png'), // 女性图标
+      storeIcon: require('@/assets/image/storeUserItem/storeIcon.png'), // 跨店图标
+      timeIcon: require('@/assets/image/storeUserItem/timeIcon.png') // 时间图标
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.storeUserItem{
+  width: 65px;
+  &>p{
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    span{
+      display: inline-block;
+      margin-left:4px ;
+      line-height: 17px;
+    }
+  }
 
+}
+.storeUserItem_img{
+  margin: 0 auto;
+  position: relative;
+  text-align: center;
+  width: 58px;
+  height: 58px;
+  margin-bottom:4px ;
+}
+.userIcon{
+  width: 58px;
+  height: 58px;
+}
+// vip图标样式
+.vipIcon{
+  position: absolute;
+  bottom: 0;
+  width: 19.87px;
+  height: 14.86px;
+  right: 0;
+}
+.van-icon{
+ font-size: 12px;
+ margin-bottom:2px ;
+}
+
+// 无图标时文字左边距
+.storeUserItem>p>.noIcon{
+  margin-left:16px;
+}
 </style>
--- src/views/home/enterstore/index.vue
+++ src/views/home/enterstore/index.vue
@@ -1,7 +1,10 @@
 <!--今日进店-->
 <template>
-  <div class="enterstore">今日进店
-    <StoreUserItem v-for="item in data" :key="item.id" :data="item"/>
+  <div class="enterstore">
+    <div>今日进店</div>
+    <div class="enterstore_content">
+      <StoreUserItem v-for="item in data" :key="item.id" :data="item"/>
+    </div>
   </div>
 </template>
 
@@ -14,20 +17,26 @@ export default {
         {
           id: 1,
           // icon: require(),
+          date: '19:44:15',
           age: '35~50岁',
-          count: 0,
-          vip: false
+          count: 2, // 到店次数
+          vip: false,
+          crossStore: true, // 跨店
+          sex: 0 // 0 女  1 男
         },
         {
           id: 2,
           // icon: require(),
+          date: '19:44:15',
           age: '20~35岁',
-          count: 2,
+          count: 1,
           vip: true
+
         },
         {
           id: 3,
           // icon: require(),
+          date: '19:44:15',
           age: '35~50岁',
           count: 6,
           vip: false
@@ -35,6 +44,7 @@ export default {
         {
           id: 4,
           // icon: require(),
+          date: '19:44:15',
           age: '35~50岁',
           count: 3,
           vip: true
@@ -52,5 +62,9 @@ export default {
 .enterstore{
   .rem(height,169px);
   background-color: #fff;
+
+.enterstore_content{
+  display: flex;
+}
 }
 </style>
