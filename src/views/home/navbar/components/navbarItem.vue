a53b3a8e3b4fc4435507d85fa621ea4cd57ac572
LZ <2706253331@qq.com>
2021-03-05 10:58:54 +0800

    完成navbarItem组件封装

--- src/views/home/navbar/components/navbarItem.vue
+++ src/views/home/navbar/components/navbarItem.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-每一个菜单栏选项
+  <div class="navbarItem" @click="jump">
+    <van-icon :name="data.icon" :badge="data.unread < 1 ? '' : data.unread>99 ? '99+' : data.unread" />
+    <p>{{data.name}}</p>
   </div>
 </template>
 
@@ -10,10 +11,40 @@ export default {
     data: {
       type: Object
     }
+  },
+  data () {
+    return {
+      baoJing: require('@/assets/image/home/navbar/baoJing.png') // 报警
+
+    }
+  },
+  methods: {
+    jump () {
+      this.$emit('jump')
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
+.navbarItem{
+  .rem(width, 50px);
+  text-align: center;
+  &>p{
+    .rem(font-size, 12px);
+    color: #000000;
+    .rem(line-height, 17px);
+    .rem(margin-top, 10px) ;
+  }
+  &>.van-icon{
+  .rem(font-size, 44px);
+  }
+  .van-info{
+    .rem(padding, 2px);
+    .rem(padding-left, 6px);
+    .rem(padding-right, 6px) ;
+    .rem(border-radius, 10px);
+    border: none;
+  }
+}
 </style>
